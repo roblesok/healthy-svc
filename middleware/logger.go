115b6bdc8f29dee13df5730a6f17484af51f72f6
Robles AR <tinolabs@yandex.com>
2021-01-18 22:28:28 -0300

    move recovery to separate file

--- middleware/logger.go
+++ middleware/logger.go
@@ -18,18 +18,3 @@ func Logger(next http.Handler) http.Handler {
 	return http.HandlerFunc(fn)
 
 }
-
-func Recovery(next http.Handler) http.Handler {
-	fn := func(w http.ResponseWriter, r *http.Request) {
-		defer func() {
-			if err := recover(); err != nil {
-				log.Printf("panic: %+v", err)
-				http.Error(w, http.StatusText(500), 500)
-			}
-		}()
-
-		next.ServeHTTP(w, r)
-	}
-
-	return http.HandlerFunc(fn)
-}
